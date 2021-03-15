e2e1d985b727c984a5eb9dbe8c3c92c810d1e17a
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2021-03-15 12:33:38 +0800

    refactor

--- README.md
+++ README.md
@@ -1,30 +1,11 @@
-# Augmented Reality Feature Matching using the NDK with an async approach for >= Android 4.0
+## feature-matching-native
+This repo is fork from [michaeltroger/feature-matching-native](https://github.com/michaeltroger/feature-matching-native). Take some refactor and debug it.
 
-<img src="/featurematchingnative.png" alt="Feature matching native" width="400px"/>
+![](/screenshot/demo.gif)
 
-### What is this repository for?
-* Uses the camera image to search for a specified template image within it via a feature matching approach using the OpenCV C++ library. The detected object is marked with lines within the scene. This can be used to e.g. find a logo.
-* uses an async approach, that means that not every frame will be searched for the object and the GUI stays fluent/responsible
-* the OpenCV Java library is used as well for loading the template image. The processing on the other hand is done by the OpenCV C++ library only. 
-* currently the async calculated image is painted over the live camera preview which can be seen on the edge. This might be changed in the future.
-* Version 1.1
 
-### How do I get set up?
-* IDE: Android Studio (tested with 2.2.2)
-* Android SDK & NDK
-* Dependencies: OpenCV 3.0.0 library (included)
-* Template image location: res/drawable Changeable in CameraPreviewView
-* Make sure the app has the required permission on start, as there is no runtime-check yet! (Camera)
 
-### Default template image
-<img src="/app/src/main/res/drawable/coca_cola.bmp" alt="" width="200px"/>
-
-Copyright of the logo: The Coca-Cola Company
-
-### Who do I talk to?
-* Repo owner and developer: android@michaeltroger.com
-
-### Credits
-* The passing of the camera frame data from Java to C++ and the async approach is based on Jay Rambhia's AsyncCamera  https://github.com/jayrambhia/AsynCamera
-* The feature matching is based on this official OpenCV tutorial http://docs.opencv.org/2.4/doc/tutorials/features2d/feature_homography/feature_homography.html Unlike in this application their version is using OpenCV 2 and is for use with normal images
-* This C++ library integration builds on code from the Android Open Source Project: https://github.com/googlesamples/android-ndk/tree/master/hello-libs
+## Referenc
+- michaeltroger/feature-matching-native https://github.com/michaeltroger/feature-matching-native
+- How to configure OpenCV in Android Studio 4.1 or higher https://www.youtube.com/watch?v=-0Yx1UzozzQ
+- OpenCV – 4.0.1 https://sourceforge.net/projects/opencvlibrary/files/4.0.1/opencv-4.0.1-android-sdk.zip/download
---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,170 @@
+//#include <jni.h>
+//#include <string>
+//
+//extern "C" JNIEXPORT jstring JNICALL
+//Java_com_example_myapplication_MainActivity_stringFromJNI(
+//        JNIEnv* env,
+//        jobject /* this */) {
+//    std::string hello = "Hello from C++";
+//    return env->NewStringUTF(hello.c_str());
+//}
+
+#include <jni.h>
+#include <android/log.h>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/core/types_c.h>
+#include <opencv2/imgproc/types_c.h>
+#include <opencv2/calib3d/calib3d_c.h>
+
+using namespace std;
+using namespace cv;
+
+#define  LOG_TAG    "native_opencv"
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+#define  LOGW(...)  __android_log_print(ANDROID_LOG_WARN,LOG_TAG,__VA_ARGS__)
+#define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+
+Mat* mCanny = NULL;
+
+const Scalar RGBA_DRAWING_COLOR = Scalar(0, 255, 0, 255);
+
+Ptr<ORB> fd_de;
+BFMatcher dm(NORM_HAMMING);
+
+Mat templ;
+
+vector<KeyPoint> keypointsTemplate;
+Mat descriptorsTemplate;
+
+vector<KeyPoint> keypointsCamera;
+Mat descriptorsCamera;
+
+vector<DMatch> matches;
+vector<Point2f> obj_corners(4);
+
+extern "C" {
+void Java_com_example_myapplication_views_CameraPreviewView_InitTemplateImage(
+        JNIEnv *env, jobject instance, jlong addrInputImage
+) {
+    templ = *(Mat*)addrInputImage;
+
+    LOGD( "called initTemplate %d x %d", templ.cols, templ.rows );
+
+    fd_de = ORB::create();
+
+    if (!templ.empty())
+        LOGD("not empty");
+
+    fd_de->detectAndCompute(templ, Mat(), keypointsTemplate, descriptorsTemplate);
+
+    //-- Get the corners from the image_1 ( the object to be "detected" )
+    obj_corners[0] = cvPoint(0,0);
+    obj_corners[1] = cvPoint( templ.cols, 0 );
+    obj_corners[2] = cvPoint( templ.cols, templ.rows );
+    obj_corners[3] = cvPoint( 0, templ.rows );
+}
+
+jboolean Java_com_example_myapplication_views_CameraPreviewView_ImageProcessing(
+        JNIEnv* env, jobject thiz,
+        jint width, jint height,
+        jbyteArray NV21FrameData, jintArray outPixels
+) {
+    jbyte* pNV21FrameData = env->GetByteArrayElements(NV21FrameData, 0);
+    jint* poutPixels = env->GetIntArrayElements(outPixels, 0);
+
+
+    if ( mCanny == NULL )
+    {
+        LOGD( "created mCanny" );
+        mCanny = new Mat(height, width, CV_8UC1);   // single channel array with 8 bit unsigned integers
+    }
+
+    Mat mGray(height, width, CV_8UC1, (unsigned char*)pNV21FrameData); // single channel array with 8 bit unsigned integers
+    Mat yuvImg(height+height/2, width, CV_8UC1, (unsigned char *)pNV21FrameData);
+    Mat rgbaImg;
+    cvtColor(yuvImg, rgbaImg, CV_YUV2RGBA_NV21);
+
+
+    Mat mResult(height, width, CV_8UC4, (unsigned char*)poutPixels);   // 4 channel array with 8 bit unsigned integers
+
+    fd_de->detectAndCompute(mGray, Mat(), keypointsCamera, descriptorsCamera);
+
+    if (!descriptorsCamera.empty()) {
+        dm.match(descriptorsTemplate, descriptorsCamera, matches);
+
+        if (matches.size() >= 4) {
+            float max_dist = 0;
+            float min_dist = 100;
+
+            //-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
+            vector<DMatch> good_matches;
+
+            for(int i = 0; i < descriptorsTemplate.rows; i++ )
+            {
+                if (matches[i].distance < 3 * min_dist)
+                {
+                    good_matches.push_back(matches[i]);
+                }
+            }
+
+            if (!good_matches.empty()) {
+                //-- Localize the object
+                vector<Point2f> obj;
+                vector<Point2f> scene;
+
+                for( int i = 0; i < good_matches.size(); i++ )
+                {
+                    //-- Get the keypoints from the good matches
+                    obj.push_back( keypointsTemplate[ good_matches[i].queryIdx ].pt );
+                    scene.push_back( keypointsCamera[ good_matches[i].trainIdx ].pt );
+                }
+                Mat H = findHomography( obj, scene, CV_RANSAC, 10 );
+
+
+                vector<Point2f> scene_corners(4);
+                try {
+                    perspectiveTransform( obj_corners, scene_corners, H);
+
+                    line( rgbaImg,
+                          scene_corners[0],
+                          scene_corners[1],
+                          RGBA_DRAWING_COLOR,
+                          4 );
+                    line( rgbaImg,
+                          scene_corners[1],
+                          scene_corners[2],
+                          RGBA_DRAWING_COLOR,
+                          4 );
+                    line( rgbaImg,
+                          scene_corners[2],
+                          scene_corners[3],
+                          RGBA_DRAWING_COLOR,
+                          4 );
+                    line( rgbaImg,
+                          scene_corners[3],
+                          scene_corners[0],
+                          RGBA_DRAWING_COLOR,
+                          4 );
+                }
+
+                catch (Exception e) {
+                    // ignore for now
+                }
+            }
+
+        }
+    }
+
+    cvtColor(rgbaImg, mResult, CV_RGBA2BGRA);
+
+    env->ReleaseByteArrayElements(NV21FrameData, pNV21FrameData, 0);
+    env->ReleaseIntArrayElements(outPixels, poutPixels, 0);
+
+    return JNI_TRUE;
+}
+}
