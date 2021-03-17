f2d006dff054dac28c9bcd8c77f8d05716adb181
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2021-03-17 16:44:39 +0800

    add ask camera premission

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,29 +15,6 @@ import android.widget.ImageView;
 import com.example.myapplication.tools.CameraManager;
 import com.example.myapplication.views.CameraPreviewView;
 
-//public class MainActivity extends AppCompatActivity {
-//
-//    // Used to load the 'native-lib' library on application startup.
-//    static {
-//        System.loadLibrary("native-lib");
-//    }
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        // Example of a call to a native method
-//        TextView tv = findViewById(R.id.sample_text);
-//        tv.setText(stringFromJNI());
-//    }
-//
-//    /**
-//     * A native method that is implemented by the 'native-lib' native library,
-//     * which is packaged with this application.
-//     */
-//    public native String stringFromJNI();
-//}
 /**
  * main activity, responsible for loading the layout and its views
  * @author Michael Troger
@@ -58,6 +39,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         // get a camera manager instance
         mCameraManager = new CameraManager(this);
         ImageView myCameraPreview = new ImageView(this);
@@ -78,7 +60,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onPause() {
         super.onPause();
-
         mPreview.onPause();
         mCameraManager.onPause();
 
@@ -89,11 +70,17 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-        mCameraManager.onResume();
+        // ask camera permission
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.CAMERA}, 1);
+        }else{
+            mCameraManager.onResume();
 
-        mPreview.setCamera(mCameraManager.getCamera());
+            mPreview.setCamera(mCameraManager.getCamera());
 
-        mPreview.setVisibility(View.VISIBLE); // this can fix the freeze.
+            mPreview.setVisibility(View.VISIBLE); // this can fix the freeze.
+        }
+
     }
 
 }
